905c35d7a28c1798f6bf34a4631769fb12cbc5da
Konstantin <bob@uni-mainz.de>
2021-06-17 18:28:29 +0200

    ROC by LSH.

---
+++ src/python/roc/LSHonSynthetics.py
@@ -0,0 +1,42 @@
+import subprocess
+import time
+import pandas as pd
+import numpy as np
+import sys
+
+def run(k,l):
+    start = time.time()
+    # ./mzBucket -f 1 -k 20 -l 20 -c synthetics.csv -t 32
+    runCommand = ["/share/massSpec/syntheticsLSH/mzBucket","-f","1","-k","{}".format(k),"-l","{}".format(l),"-c","/share/massSpec/syntheticsLSH/synthetics.csv","-t","16"]
+    print(runCommand)
+    res = subprocess.run(runCommand)
+    print(res)
+    return res
+
+# outFile
+outfile = sys.argv[1]
+
+# run parameters
+ampParameters = [(30,64),(30,30),(30,22)]
+
+# store results
+runTime = []
+kList = []
+lList = []
+threadsUsed = []
+for nt in np.flip(numThreads):
+    for (l,k) in ampParameters:
+        duration = run(nt,l,k) 
+        #print("l {} k {} time {}".format(l,k,duration))
+        #runTime.append(duration)
+        #kList.append(k)
+        #lList.append(l)
+        #threadsUsed.append(nt)
+        #with open("{}_tmp".format(outfile), 'a') as the_file:
+        #    the_file.write('{},{},{},{}\n'.format(nt,duration,l,k))
+
+#df = pd.DataFrame.from_dict({'numThreads':threadsUsed,'runTimeSec':runTime,'k':kList,'l':lList})
+#df.to_csv(outfile)
+#print(df)
+
+
