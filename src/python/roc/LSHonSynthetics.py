d4d4a6458519b060d928f96ff57ab75d35338c76
Konstantin Bob <bob@informatik.uni-mainz.de>
2021-06-18 10:37:01 +0200

    Roc by LSH.

--- src/python/roc/LSHonSynthetics.py
+++ src/python/roc/LSHonSynthetics.py
@@ -1,40 +1,54 @@
-import subprocess
+import subprocess as sp
 import time
 import pandas as pd
 import numpy as np
 import sys
+import re
+from tqdm import tqdm
 
 def run(l,k):
     start = time.time()
     # ./mzBucket -f 1 -k 20 -l 20 -c synthetics.csv -t 32
-    runCommand = ["/share/massSpec/syntheticsLSH/mzBucket","-f","1","-k","{}".format(k),"-l","{}".format(l),"-c","/share/massSpec/syntheticsLSH/synthetics.csv","-t","16","-v","false"]
-    print("// {}".format(runCommand))
-    res = subprocess.run(runCommand)
-    return res
+    runCommand = ["../../cpp/mzBucket","-f","1","-k","{}".format(k),"-l","{}".format(l),"-c","../../../data/synthetics.csv","-t","32","-v","false"]
+    #print("// {}".format(runCommand))
+    res = str(sp.check_output(runCommand))
+    #print("res {}".format(res))
+    counts = re.findall(r'\d+', res)
+    #print("counts {}".format(counts))
+    tp = int(counts[0])
+    tn = int(counts[1])
+    fp = int(counts[2])
+    fn = int(counts[3])
+    return (tp,fp,tn,fn)
 
 # outFile
 outfile = sys.argv[1]
 
 # run parameters
-ampParameters = [(30,22)]
+ampParameters = [(30, 25),(30, 28),(30, 30),(30, 22),(30, 32),(30, 38),(30, 45),(30, 64)]
+#ampParameters = [(30,25)]
 
 # store results
-runTime = []
 kList = []
 lList = []
-threadsUsed = []
-for (l,k) in ampParameters:
-    duration = run(l,k) 
-        #print("l {} k {} time {}".format(l,k,duration))
-        #runTime.append(duration)
-        #kList.append(k)
-        #lList.append(l)
-        #threadsUsed.append(nt)
-        #with open("{}_tmp".format(outfile), 'a') as the_file:
-        #    the_file.write('{},{},{},{}\n'.format(nt,duration,l,k))
+tprList = []
+fprList = []
+for (l,k) in tqdm(ampParameters):
+    (tp,fp,tn,fn) = run(l,k) 
+ 
+    tpr = tp / (tp + fn)
+    fpr = fp / (fp + tn)
 
-#df = pd.DataFrame.from_dict({'numThreads':threadsUsed,'runTimeSec':runTime,'k':kList,'l':lList})
-#df.to_csv(outfile)
-#print(df)
+    kList.append(k)
+    lList.append(l)
+    tprList.append(tpr)
+    fprList.append(fpr)
+
+    with open("{}_tmp".format(outfile), 'a') as the_file:
+            the_file.write('{},{},{},{}\n'.format(l,k,tpr,fpr))
+
+df = pd.DataFrame.from_dict({'l':lList,'k':kList,'tpr':tprList,'fpr':fprList})
+df.to_csv(outfile)
+print(df)
 
 
